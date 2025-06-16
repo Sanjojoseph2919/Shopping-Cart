e8f1401a2dd9a325bf55b3cd1438215bbb44c261
Sanjojoseph2919 <Sanjojoseph6959@gmail.com>
2025-06-16 14:10:31 +0530

    just remove faltu line from product-helpers

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,62 +1,7 @@
-// var db=require('../config/connection')
-// const db = require('../config/connection');
-// module.exports={
-
-//     addProduct:(product,callback)=>{
-//         console.log(product);
-
-//         let db = require('../config/connection'); 
-//          let productCollection = db.get().collection('product');
-
-
-//         db.get().collection('product').insertOne(product).then((data)=>{
-//             console.log(data)
-//             callback(data.ops[0].id)
-//         })
-//     }
-
-// }
-
-// const db = require('../config/connection');
-// var collections = require('../config/collections')
-// module.exports = {
-//     addProduct: (product, callback) => {
-//         console.log("Received product:", product);
-
-//         const database = db.get();
-//         if (!database) {
-//             console.error("Database connection is not established!");
-//             return callback("Database not connected");
-//         }
-
-//         database.collection('product').insertOne(product)
-//             .then((data) => {
-//                 console.log("Product added successfully:", data);
-//                 callback(null, data);
-//             })
-//             .catch((err) => {
-//                 console.error("Error inserting product:", err);
-//                 callback(err);
-//             });
-//     },
-
-//     getAllProducts: () => {
-//         return new Promise((resolve, reject) => {
-//             let products = awaitdb.get().collection(collections.PRODUCT_COLLECTION).find().toArray()
-//             resolve(products)
-//         })
-//     }
- 
-// }
-
 var db = require('../config/connection');
 
 module.exports = {
-    // addProduct: (product, callback) => {
-    //     db.get().collection('product').insertOne(product).then((data) => {
-    //         callback(data.insertedId); // Use insertedId instead of data.ops[0]._id
-    //     });
-    // },
+    
     addProduct: (product, callback) => {
   db.get().collection('product').insertOne(product).then((data) => {
     callback(data.insertedId.toString()); // RETURN the _id as string
--- routes/admin.js
+++ routes/admin.js
@@ -10,8 +10,7 @@ router.get('/', (req, res) => {
     console.log(products)
     res.render('admin/views-products', { admin: true, products });
   })
- 
-
+  
 });  
 
 
